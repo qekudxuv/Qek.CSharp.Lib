a8aa3a2d94e7459104fd36e227d5b1e30df3898d
sam.sh_chang <sam.sh_chang@htc.com>
2017-08-11 18:12:31 +0800

    add Qek.DB project

---
+++ Qek.DB/TranScopeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Transactions;
+
+namespace Qek.DB
+{
+    public class TranScopeHelper
+    {
+        public static TransactionScope GetTranScope(
+            TransactionScopeOption tsOption = TransactionScopeOption.Required,
+            int timeoutSec = 3600,
+            IsolationLevel isoLevel = IsolationLevel.ReadCommitted
+            )
+        {
+            return new TransactionScope(
+                    tsOption,
+                    new TransactionOptions
+                    {
+                        Timeout = TimeSpan.FromSeconds(timeoutSec),
+                        IsolationLevel = isoLevel
+                    });
+        }
+    }
+}

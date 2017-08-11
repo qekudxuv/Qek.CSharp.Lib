0d978b21afc716bb6f9c8887abaf73fc57049b99
sam.sh_chang <sam.sh_chang@htc.com>
2017-08-11 19:35:15 +0800

    add Qek.ExtendMethod project

---
+++ Qek.ExtendMethod/ExtEnumerable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Qek.ExtendMethod
+{
+    public static class ExtEnumerable
+    {
+        public static bool IsNullOrEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            return source == null || !source.Any();
+        }
+    }
+}
---
+++ Qek.ExtendMethod/ExtSession.cs
@@ -0,0 +1,91 @@
+﻿using NHibernate;
+using NHibernate.Engine;
+using NHibernate.Persister.Entity;
+using NHibernate.Proxy;
+using System;
+using System.Linq;
+
+namespace Qek.ExtendMethod
+{
+    public static class ExtSession
+    {
+        /// <summary>
+        /// Determines whether [is dirty entity] [the specified entity].
+        /// ref : 
+        /// http://nhibernate.info/doc/howto/various/finding-dirty-properties-in-nhibernate.html
+        /// http://stackoverflow.com/questions/2820660/get-name-of-property-as-a-string
+        /// </summary>
+        /// <param name="session">The session.</param>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        public static Boolean IsDirtyEntity(this ISession session, Object entity)
+        {
+            ISessionImplementor sessionImpl = session.GetSessionImplementation();
+            IPersistenceContext persistenceContext = sessionImpl.PersistenceContext;
+            EntityEntry oldEntry = persistenceContext.GetEntry(entity);
+            String className = oldEntry.EntityName;
+            IEntityPersister persister = sessionImpl.Factory.GetEntityPersister(className);
+
+            if ((oldEntry == null) && (entity is INHibernateProxy))
+            {
+                INHibernateProxy proxy = entity as INHibernateProxy;
+                Object obj = sessionImpl.PersistenceContext.Unproxy(proxy);
+                oldEntry = sessionImpl.PersistenceContext.GetEntry(obj);
+            }
+
+            Object[] oldState = oldEntry.LoadedState;
+            Object[] currentState = persister.GetPropertyValues(entity, sessionImpl.EntityMode);
+            Int32[] dirtyProps = oldState.Select((o, i) => (oldState[i] == currentState[i]) ? -1 : i).Where(x => x >= 0).ToArray();
+
+            return (dirtyProps != null);
+        }
+
+        public static Boolean IsDirtyProperty(this ISession session, Object entity, String propertyName)
+        {
+            ISessionImplementor sessionImpl = session.GetSessionImplementation();
+            IPersistenceContext persistenceContext = sessionImpl.PersistenceContext;
+            EntityEntry oldEntry = persistenceContext.GetEntry(entity);
+            String className = oldEntry.EntityName;
+            IEntityPersister persister = sessionImpl.Factory.GetEntityPersister(className);
+
+            if ((oldEntry == null) && (entity is INHibernateProxy))
+            {
+                INHibernateProxy proxy = entity as INHibernateProxy;
+                Object obj = sessionImpl.PersistenceContext.Unproxy(proxy);
+                oldEntry = sessionImpl.PersistenceContext.GetEntry(obj);
+            }
+
+            Object[] oldState = oldEntry.LoadedState;
+            Object[] currentState = persister.GetPropertyValues(entity, sessionImpl.EntityMode);
+            Int32[] dirtyProps = persister.FindDirty(currentState, oldState, entity, sessionImpl);
+            Int32 index = Array.IndexOf(persister.PropertyNames, propertyName);
+
+            Boolean isDirty = (dirtyProps != null) ? (Array.IndexOf(dirtyProps, index) != -1) : false;
+            return (isDirty);
+        }
+
+        public static Object GetOriginalEntityProperty(this ISession session, Object entity, String propertyName)
+        {
+            ISessionImplementor sessionImpl = session.GetSessionImplementation();
+            IPersistenceContext persistenceContext = sessionImpl.PersistenceContext;
+            EntityEntry oldEntry = persistenceContext.GetEntry(entity);
+            String className = oldEntry.EntityName;
+            IEntityPersister persister = sessionImpl.Factory.GetEntityPersister(className);
+
+            if ((oldEntry == null) && (entity is INHibernateProxy))
+            {
+                INHibernateProxy proxy = entity as INHibernateProxy;
+                Object obj = sessionImpl.PersistenceContext.Unproxy(proxy);
+                oldEntry = sessionImpl.PersistenceContext.GetEntry(obj);
+            }
+
+            Object[] oldState = oldEntry.LoadedState;
+            Object[] currentState = persister.GetPropertyValues(entity, sessionImpl.EntityMode);
+            Int32[] dirtyProps = persister.FindDirty(currentState, oldState, entity, sessionImpl);
+            Int32 index = Array.IndexOf(persister.PropertyNames, propertyName);
+
+            Boolean isDirty = (dirtyProps != null) ? (Array.IndexOf(dirtyProps, index) != -1) : false;
+            return ((isDirty == true) ? oldState[index] : currentState[index]);
+        }
+    }
+}
